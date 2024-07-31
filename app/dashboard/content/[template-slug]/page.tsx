03faf5764cb057f4512e2d8a723718d86ba753d0
Megh Mehta <mehtamegh@gmail.com>
2024-07-31 15:08:28 -0400

    Dynamic routes

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import FormSection from "../_components/FormSection";
 import OutputSection from "../_components/OutputSection";
+import { TEMPLATE } from "../../_components/TemplateListSection";
+import Templates from "@/app/(data)/Templates";
 
 interface PROPS {
   params: {
@@ -9,10 +11,14 @@ interface PROPS {
 }
 
 function CreateNewContent(props: PROPS) {
+  const selectedTemplate: TEMPLATE | undefined = Templates?.find(
+    (item) => item.slug == props.params["template-slug"]
+  );
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-10 p-5">
       {/* FormSection */}
-      <FormSection />
+      <FormSection selectedTemplate={selectedTemplate} />
       {/* OutputSection */}
       <OutputSection />
     </div>
--- app/dashboard/content/_components/FormSection.tsx
+++ app/dashboard/content/_components/FormSection.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { TEMPLATE } from "../../_components/TemplateListSection";
+import Image from "next/image";
 
-function FormSection() {
-  return (
-    <div>FormSection</div>
-  )
+interface PROPS {
+  selectedTemplate?: TEMPLATE;
 }
 
-export default FormSection
+function FormSection({ selectedTemplate }: PROPS) {
+  return (
+    <div className="p-5 shadow-md border rounded-lg">
+      <Image src={selectedTemplate?.icon} alt="icon" width={50} height={50} />
+      <h2 className="font-boldtext-2xl mb-2 text-primary">
+        {selectedTemplate?.name}
+      </h2>
+      <p className="text-gray-500 text-sm">{selectedTemplate?.desc}</p>
+      <form>
+        {}
+      </form>
+    </div>
+  );
+}
+
+export default FormSection;
