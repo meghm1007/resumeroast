da7b5a010de941de53486893db1c88dc56066025
Megh Mehta <93192358+meghm1007@users.noreply.github.com>
2025-04-22 23:04:47 -0400

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,6 @@ export default {
   schema: "./utils/schema.tsx",
   out: "./drizzle",
   dbCredentials: {
-    url: "postgresql://resumeroast_owner:K27TCcYNfQAm@ep-muddy-wave-a5boo08j.us-east-2.aws.neon.tech/resumeroast?sslmode=require",
+    url: "",
   },
 };
